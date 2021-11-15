5aa2ec317b64a1d98fc79588cc440b75352f822e
Jo Wagner <jowagner@limago.de>
2021-11-15 14:01:35 +0100

    Initial commit

---
+++ springdemo/src/main/java/de/application/Main.java
@@ -0,0 +1,27 @@
+package de.application;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import de.application.hello.Hello;
+
+
+@ComponentScan("de")
+public class Main {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		//AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		AbstractApplicationContext context =new AnnotationConfigApplicationContext(Main.class);
+		context.registerShutdownHook();
+
+
+	}
+
+}
